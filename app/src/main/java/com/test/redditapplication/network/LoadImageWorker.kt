b6da1af750649cbc17c5d70575b72296e9616d87
Yevgenii Shein <initbin@gmail.com>
2019-05-28 09:24:50 +0300

    Fix media saving

--- app/src/main/java/com/test/redditapplication/network/LoadImageWorker.kt
+++ app/src/main/java/com/test/redditapplication/network/LoadImageWorker.kt
@@ -1,11 +1,10 @@
 package com.test.redditapplication.network
 
 import android.content.Context
-import android.os.Environment
+import android.provider.MediaStore
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.test.redditapplication.POST_ID_KEY
-import com.test.redditapplication.R
 import com.test.redditapplication.db.TopPostsDao
 import com.test.redditapplication.getPostDao
 import java.io.File
@@ -15,13 +14,7 @@ class LoadImageWorker(context: Context, workerParams: WorkerParameters) : Worker
 
     private val postsDao: TopPostsDao by lazy { context.getPostDao() }
 
-    override fun getFolder(): File {
-        val mainDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        val folderName = applicationContext.getString(R.string.app_name)
-        return File(mainDirectory, folderName).apply {
-            if (!exists()) mkdirs()
-        }
-    }
+    override fun getFolder(): File = applicationContext.cacheDir
 
     override fun doWork(): Result {
         val id = inputData.getString(POST_ID_KEY) ?: return Result.failure()
@@ -29,8 +22,9 @@ class LoadImageWorker(context: Context, workerParams: WorkerParameters) : Worker
         val path = loadImage(post.imageUrl)
 
         return if (path.isNotBlank()) {
-            addImageToGallery(path)
+            MediaStore.Images.Media.insertImage(applicationContext.contentResolver, path, post.title, post.title)
             postsDao.insert(listOf(post.copy(savedToMedia = true)))
+            File(path).delete()
             Result.success()
         } else {
             Result.failure()
